803d5e1f391c28e3daa76439171bd7b17d2e4b51
Dytonis <dytonis123@gmail.com>
2017-12-02 09:38:09 -0700

    day 1 and a half

---
+++ Assets/Scripts/LetterGradeCalculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LD40
+{
+    public class LetterGradeCalculator
+    {
+        public static LetterGrade GetGradeFromScore(float time, float ATime, int shots, int AShots)
+        {
+            float timescore = (ATime / time) * (float)LetterGrade.A;
+
+            float shotscore = (AShots / shots) * (float)LetterGrade.A;
+
+            float score = (timescore + shotscore) / 2;
+
+            if (score >= 130)
+                return LetterGrade.SS;
+            else if (score >= 120)
+                return LetterGrade.S;
+            else if (score >= 110)
+                return LetterGrade.AA;
+            else if (score >= 100)
+                return LetterGrade.A;
+            else if (score >= 80)
+                return LetterGrade.B;
+            else if (score >= 60)
+                return LetterGrade.C;
+            else if (score >= 40)
+                return LetterGrade.D;
+            else return LetterGrade.F;
+        }
+    }
+
+    public enum LetterGrade
+    {
+        SS = 130,
+        S = 120,
+        AA = 110,
+        A = 100,
+        B = 80,
+        C = 60,
+        D = 40,
+        F = 0,
+    }
+}
